151ca2ec51914b4caaaf76ca4b3620a16e7267d8
Ritik Nandanwar <59316289+RITIK-111@users.noreply.github.com>
2022-02-22 22:25:42 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,15 @@
+
+def add(x,y):
+	pass
+def sub(x,y):
+	return x-y # on main branch 
+def mult(x,y):
+	pass  # on bug56
+def div(x,y):
+	if y == 0 :
+		return divide_by_o_error
+	else:
+		return x/y
+
+def square(x):
+	pass
